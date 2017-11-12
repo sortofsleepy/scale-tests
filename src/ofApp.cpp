b6d5b5d99df1b2c0ca390250e10e6a26c50280be
Joe <sortofsleepy@gmail.com>
2017-11-11 20:09:11 -0800

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,138 +1,154 @@
 #include "ofApp.h"
+//! vertex data to render the camera image
+float kImagePlaneVertexData[8] = {
+	-1.0, -1.0,  
+	1.0, -1.0,  
+	-1.0,  1.0, 
+	1.0,  1.0
+};
 
+float verts[16] = {
+	-1.0, -1.0,  0.0, 1.0,
+	1.0, -1.0,  1.0, 1.0,
+	-1.0,  1.0,  0.0, 0.0,
+	1.0,  1.0,  1.0, 0.0,
+};
+
+float texCoords[8] = {
+	0.0, 1.0,
+	1.0,1.0,
+	0.0,0.0,
+	1.0,0.0
+};
+
+float aspect = 0.0f;
+float diffX = 0.0f;
+float diffY = 0.0f;
+ofVec2f aspectRatio;
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 	ofDisableArbTex();
 	image.load("image.jpg");
 
-	cap = ofRectangle(0, 0, captureWidth, captureHeight);
+
+	cap = ofRectangle(0, 0, image.getWidth(),image.getHeight());
 	screen = ofRectangle(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
 
-	cap.scaleTo(screen, OF_ASPECT_RATIO_KEEP);
-	cap.scaleFromCenter(ofPoint(2, 2));
-	ofLog() << cap.getCenter();
-	fbo.allocate(cap.getWidth(),cap.getHeight(),GL_RGBA);
+	aspect = 1;
 
+	ofLog() << (float)ofGetWindowWidth() / (float)ofGetWindowHeight();
+
+	float ratio = (float)ofGetWindowWidth() / (float)ofGetWindowHeight();
+
+
+	aspectRatio.x = (float)aspect;
+	aspectRatio.y = (float)aspect;
+
+
+
+	// 2 should instead be the scale factor from [[UIScreen mainScreen] scale]
+	// or maybe aspect ratio?
+	cap.scaleFromCenter(ofPoint(ratio,ratio));
+
+	diffX = cap.getWidth() - ofGetWindowWidth();
+	diffY = cap.getHeight() - ofGetWindowHeight();
+	diffX /= 2.0;
+	diffY /= 2.0;
+
+	//vMesh.setVertexData(kImagePlaneVertexData, 4, 8, GL_DYNAMIC_DRAW);
+
+	vMesh.setVertexData(verts, 4, 16, GL_DYNAMIC_DRAW);
+
+	vMesh.enableTexCoords();
+	vMesh.setTexCoordData(texCoords, 8, GL_DYNAMIC_DRAW);
+
+	fbo.allocate(ofGetWindowWidth(),ofGetWindowHeight(), GL_RGBA);
+	drawFbo.allocate(cap.getWidth(),cap.getHeight(), GL_RGBA);
 
 	shader.setupShaderFromSource(GL_VERTEX_SHADER, camera_convert_vertex);
-	shader.setupShaderFromSource(GL_FRAGMENT_SHADER,camera_convert_fragment);
+	shader.setupShaderFromSource(GL_FRAGMENT_SHADER, camera_convert_fragment);
+	shader.bindDefaults();
 	shader.linkProgram();
-	plane = ofMesh::plane(captureWidth, captureHeight);
+	plane = ofMesh::plane(image.getWidth(),image.getHeight());
 
-
-	/*
-	renderShader.setupShaderFromSource(GL_VERTEX_SHADER, render_vertex);
-	renderShader.setupShaderFromSource(GL_FRAGMENT_SHADER, render_fragment);
-	renderShader.linkProgram();
-	renderPlane = ofMesh::plane(ofGetWindowWidth(),ofGetWindowHeight());*/
-
+	
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-	
-	fbo.begin();
+void ofApp::update() {
+
+
+	drawFbo.begin();
 	shader.begin();
 	shader.setUniformTexture("image", image.getTexture(), 0);
-	plane.draw();
+	shader.setUniform2f("aspectRatio", aspectRatio.x,aspectRatio.y);
+	vMesh.draw(GL_TRIANGLE_STRIP, 0, 16);
 	shader.end();
+	drawFbo.end();
+
+	fbo.begin();
+	drawFbo.draw(-diffX, -diffY);
 	fbo.end();
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-	ofTranslate(0, 0);
-	ofPoint center = cap.getCenter();
-
-	float x = center.x;
-	float y = center.y / 2;
-	fbo.draw(-x,-y, cap.getWidth(), cap.getHeight());
-
-	// maybe? 
-	// * seem to recall portrait still being messed up
-	// * also zooming is an issue potentially?
-	//fbo.draw((cap.getWidth() / 2) * -1,0,cap.getWidth() * 2,cap.getHeight() * 2);
-
-	/*
-		Maybe above for portrait, and this for landscape
-
-		fbo.draw((cap.getWidth() / 2) * -1, (cap.getHeight() / 4) * -1, cap.getWidth() * 2, cap.getHeight() * 2);
-
-	*/
-
-
-	/*
-		Using a rendering plane to avoid altering the texture as expected, has a similar effect
-		due to using full screen uv settings in the shader.
-	*/
-
-	/*
-
-	Could work? Need to figure out what default shader uniforms are + another issue is re-allocation of mesh
+void ofApp::draw() {
 	
-		float x = cap.getWidth() / 2;
-	x *= -1;
-	float aspect = cap.getWidth() / cap.getHeight();
-	aspect *= 0.1;
-	renderShader.begin();
-	renderShader.setUniform1f("aspect",aspect);
-	renderShader.setUniformTexture("image", fbo.getTexture(), 0);
-	renderPlane.draw();
-	renderShader.end();
-	*/
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+	fbo.draw(0,0,ofGetWindowWidth(),ofGetWindowHeight());
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
